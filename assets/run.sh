cea7633e6343e9b06c775fbfa28b957555378350
Sebastian Ruml <sebastian.ruml@gmail.com>
2014-11-01 18:09:46 +0100

    some small changes

--- assets/run.sh
+++ assets/run.sh
@@ -2,6 +2,7 @@
 
 INSTALL_DIR=/home/gogs/gogs
 DATA_DIR=/data/gogs
+GIT_REPO_DIR=/data/git
 
 SETUP_DIR=/app/setup
 SETUP_CONF_DIR=/app/setup/config
@@ -27,20 +28,19 @@ fi
 # Create required folders
 if ! test -d ${DATA_DIR}
 then
-    mkdir -p ${DATA_DIR}/data ${DATA_DIR}/log ${DATA_DIR}/conf ${DATA_DIR}/../git
+    mkdir -p ${DATA_DIR}/data ${DATA_DIR}/log ${DATA_DIR}/conf ${GIT_REPO_DIR}
 fi
 
 # fix permission and ownership of ${DATA_DIR}
 chmod 755 ${DATA_DIR}
 chown git:git ${DATA_DIR} -R
-
-cd ${INSTALL_DIR}
+chown git:git ${GIT_REPO_DIR} -R
 
 # Add the custom configuration if its not found
 if [ ! -f ${DATA_DIR}/custom/conf/app.ini ]
 then
     mkdir -p ${DATA_DIR}/custom/conf
-    cp ${SETUP_DIR}/config/app.ini ${DATA_DIR}/custom/conf/
+    cp ${SETUP_CONF_DIR}/app.ini ${DATA_DIR}/custom/conf/
 
     # Configure database
     sudo -u git -H sed -i .bak 's/{{DB_HOST}}/'"${DB_HOST}"'/' ${DATA_DIR}/custom/conf/app.ini
@@ -52,6 +52,8 @@ then
     # Set HTTP_PORT
     sudo -u git -H sed -i .bak  's/{{HTTP_PORT}}/'"${GOGS_PORT}"'/' ${DATA_DIR}/custom/conf/app.ini
 fi
+
+cd ${INSTALL_DIR}
 
 # Create symlink to ${DATA_DIR}/custom/conf
 mkdir -p custom
